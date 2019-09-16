c3e2b57386a92304dcc095d7989dfabacf7e0c8c
myan <myan@softserveinc.com>
2019-09-16 09:43:27 +0300

    - Removed not existing dependencies

--- src/main/java/org/gokareless/ws/AppController.java
+++ src/main/java/org/gokareless/ws/AppController.java
@@ -1,41 +1,45 @@
 package org.gokareless.ws;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AppController {
 
-    @RequestMapping("/release")
-    Release release() {
-        return new Release("@fservice",  "2.0");
+  @RequestMapping("/release")
+  Release release() {
+    return new Release("@fservice", "2.0");
+  }
+
+  static class Release {
+
+    String name;
+    String version;
+
+    public Release() {
     }
 
-    static class Release {
-        String name;
-        String version;
-        public Release(){};
-        public Release(String name, String version) {
-            this.name = name;
-            this.version = version;
-        }
+    ;
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
+    public Release(String name, String version) {
+      this.name = name;
+      this.version = version;
     }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public String getVersion() {
+      return version;
+    }
+
+    public void setVersion(String version) {
+      this.version = version;
+    }
+  }
 }
